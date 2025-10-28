7aef9221d12191a256ce0a85237125bf68ab98fc
Chaojie1 <chaojiew2@nycstudents.net>
2025-10-28 11:02:10 -0700

    crhfgaegaeb

--- app/src/main.js
+++ app/src/main.js
@@ -6,6 +6,11 @@ const pets = [
 pets.forEach((x) =>
   document.querySelector(".cardholder").insertAdjacentHTML(
     "afterbegin",
-    `<h1 class="card">hi</h1>`
+    `<div class="card">
+    <img class="petpic" src="pets/${x}.png" alt="${x}">
+      <h1>${x}</h1>
+      <button class="selector">hi</button>
+    </div>
+    `
   )
 );
