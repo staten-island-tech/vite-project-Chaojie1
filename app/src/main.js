966d29a3f3ece23044c8e1399ab189202712dc66
Chaojie1 <chaojiew2@nycstudents.net>
2025-10-29 10:59:24 -0700

    aehre

--- app/src/main.js
+++ app/src/main.js
@@ -1,16 +1,17 @@
-const pets = [
-    "square",
-    "circle",
-    "triangle"
-];
+const pets = ["square", "circle", "triangle"];
 pets.forEach((x) =>
   document.querySelector(".cardholder").insertAdjacentHTML(
     "afterbegin",
     `<div class="card">
-    <img class="petpic" src="pets/${x}.png" alt="${x}">
+    <img src="pets/${x}.png" alt="${x}">
       <h1>${x}</h1>
-      <button class="selector">hi</button>
+      <button class="selector">Select</button>
     </div>
     `
   )
+);
+document.querySelectorAll(".selector").forEach((x) =>
+  x.addEventListener("click", function (event) {
+    document.querySelector(".cardholder").innerHTML = "";
+  })
 );
