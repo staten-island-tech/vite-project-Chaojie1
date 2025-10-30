ff0cc14b26774a04bfcddca6fb58344dee8001ac
Chaojie1 <chaojiew2@nycstudents.net>
2025-10-30 11:02:13 -0700

    sdasdasdasds

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,4 @@
-const pets = ["square", "circle", "triangle"];
+const pets = ["Length", "Flash", "triangle"];
 pets.forEach((x) =>
   document.querySelector(".cardholder").insertAdjacentHTML(
     "afterbegin",
@@ -10,8 +10,34 @@ pets.forEach((x) =>
     `
   )
 );
+let selectedMode = undefined;
+function beginGame(mode) {
+  if (mode) {
+    if (mode === "Length") {
+      let failed = false;
+      let curnum = Math.round(Math.random() * 10).toString();
+      document
+        .querySelector(".cardholder")
+        .insertAdjacentHTML(
+          "afterbegin",
+          `<h1 class="numtorem">${curnum}</h1>`
+        );
+      setTimeout(function () {
+        document
+          .querySelector(".cardholder")
+          .querySelector(".numtorem")
+          .remove();
+        document.querySelector(".cardholder").insertAdjacentHTML("afterbegin","<h2>What was the number?</h2>");
+      }, 3500);
+    }
+  }
+}
 document.querySelectorAll(".selector").forEach((x) =>
   x.addEventListener("click", function (event) {
+    selectedMode = event.target
+      .closest(".card")
+      .querySelector("h1").textContent;
     document.querySelector(".cardholder").innerHTML = "";
+    beginGame(selectedMode);
   })
 );
