2dfd5a9facee33a4654b0a504b4f7a6eda1d9a07
Chaojie1 <chaojiew2@nycstudents.net>
2025-10-31 11:01:10 -0700

    zxc

--- app/src/main.js
+++ app/src/main.js
@@ -18,17 +18,51 @@ function beginGame(mode) {
       let curnum = Math.round(Math.random() * 10).toString();
       document
         .querySelector(".cardholder")
-        .insertAdjacentHTML(
-          "afterbegin",
-          `<h1 class="numtorem">${curnum}</h1>`
-        );
-      setTimeout(function () {
-        document
-          .querySelector(".cardholder")
-          .querySelector(".numtorem")
-          .remove();
-        document.querySelector(".cardholder").insertAdjacentHTML("afterbegin","<h2>What was the number?</h2>");
-      }, 3500);
+        .insertAdjacentHTML("afterbegin", `<h1 id="numtorem">${curnum}</h1>`);
+      document
+        .querySelector(".cardholder")
+        .insertAdjacentHTML("afterbegin", `<h1 id="countdown">5.00</h1>`);
+      let val = 5;
+      const inter = setInterval(() => {
+        document.getElementById("countdown").textContent =
+          Math.round(val * 100) / 100;
+        val -= 0.01;
+        if (val <= 0) {
+          clearInterval(inter);
+          document.getElementById("numtorem").remove();
+          document
+            .querySelector(".cardholder")
+            .insertAdjacentHTML("afterbegin", "<h2>What was the number?</h2>");
+          document.getElementById("countdown").remove();
+          document
+            .querySelector(".cardholder")
+            .insertAdjacentHTML(
+              "afterbegin",
+              '<input type="text" id="answerinput" placeholder="Enter the number">'
+            );
+          document
+            .querySelector(".cardholder")
+            .insertAdjacentHTML(
+              "afterbegin",
+              '<button id="submit">Submit</button>'
+            );
+          document
+            .getElementById("submit")
+            .addEventListener("click", function () {
+              if (
+                document.getElementById("answerinput").value.trim() === curnum
+              ) {
+                document
+                  .querySelector(".cardholder")
+                  .querySelector("h2").textContent = "CORRECT";
+              } else {
+                document
+                  .querySelector(".cardholder")
+                  .querySelector("h2").textContent = "WRONG";
+              }
+            });
+        }
+      }, 10);
     }
   }
 }
