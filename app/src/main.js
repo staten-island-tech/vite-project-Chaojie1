0c5f7d770e55a883a93c213bd9e76326cf9dcbf3
Chaojie1 <chaojiew2@nycstudents.net>
2025-11-06 11:02:13 -0800

    fgh

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,10 @@
-const pets = ["Length", "Flash", "triangle"];
+const pets = ["Length", "Cards", "Simon"];
+const cards = ["circle","square","triangle"]
 pets.forEach((x) =>
   document.querySelector(".cardholder").insertAdjacentHTML(
     "afterbegin",
     `<div class="card">
-    <img src="pets/${x}.png" alt="${x}">
+    <img src="modes/${x}.png" alt="${x}">
       <h1>${x}</h1>
       <button class="selector">Select</button>
     </div>
@@ -11,68 +12,80 @@ pets.forEach((x) =>
   )
 );
 function cyclelength(curnum) {
-  return new Promise((resolve)=>{
-  document
-    .querySelector(".cardholder")
-    .insertAdjacentHTML("afterbegin", `<h1 id="numtorem">${curnum}</h1>`);
-  document
-    .querySelector(".cardholder")
-    .insertAdjacentHTML("afterbegin", `<h1 id="countdown">5.00</h1>`);
-  let val = 1+Math.round(((curnum.length-1)/10)*100)/100;
-  const inter = setInterval(() => {
-    document.getElementById("countdown").textContent =
-      Math.round(val * 100) / 100;
-    val -= 0.01;
-    if (val <= 0) {
-      clearInterval(inter);
-      document.getElementById("numtorem").remove();
-      document
-        .querySelector(".cardholder")
-        .insertAdjacentHTML("afterbegin", "<h2>What was the number?</h2>");
-      document.getElementById("countdown").remove();
-      document
-        .querySelector(".cardholder")
-        .insertAdjacentHTML(
-          "afterbegin",
-          '<input type="text" id="answerinput" placeholder="Enter the number">'
-        );
-      document
-        .querySelector(".cardholder")
-        .insertAdjacentHTML(
-          "afterbegin",
-          '<button id="submit">Submit</button>'
-        );
-      document.addEventListener("keydown", function (event) {
-        if (event.key === "Enter"){
-        if (document.getElementById("answerinput").value.trim() === curnum) {
-          document
-            .querySelector(".cardholder")
-            .querySelector("h2").textContent = "CORRECT";
-            resolve(false)
-        } else {
-          document
-            .querySelector(".cardholder")
-            .querySelector("h2").textContent = `WRONG, the answer was ${curnum}`;
-            resolve(true)
-        }
+  return new Promise((resolve) => {
+    document
+      .querySelector(".cardholder")
+      .insertAdjacentHTML("afterbegin", `<h1 id="numtorem">${curnum}</h1>`);
+    document
+      .querySelector(".cardholder")
+      .insertAdjacentHTML("afterbegin", `<h1 id="countdown">5.00</h1>`);
+    let val = 1 + Math.round(((curnum.length - 1) / 10) * 100) / 100;
+    const inter = setInterval(() => {
+      document.getElementById("countdown").textContent =
+        Math.round(val * 100) / 100;
+      val -= 0.01;
+      if (val <= 0) {
+        clearInterval(inter);
+        document.querySelector(".cardholder").innerHTML = "";
+        document
+          .querySelector(".cardholder")
+          .insertAdjacentHTML("afterbegin", "<h2>What was the number?</h2>");
+        document
+          .querySelector(".cardholder")
+          .insertAdjacentHTML(
+            "afterbegin",
+            '<input type="text" id="answerinput" placeholder="Enter the number">'
+          );
+        document.addEventListener("keydown", function (event) {
+          if (event.key === "Enter") {
+            if (
+              document.getElementById("answerinput").value.trim() === curnum
+            ) {
+              document
+                .querySelector(".cardholder")
+                .querySelector("h2").textContent = "CORRECT";
+              resolve(false);
+            } else {
+              document
+                .querySelector(".cardholder")
+                .querySelector(
+                  "h2"
+                ).textContent = `WRONG, the answer was ${curnum}`;
+              resolve(true);
+            }
+          }
+        });
       }
-      });
-    }
-  }, 10);
-});
+    }, 10);
+  });
 }
 let selectedMode = undefined;
 async function beginGame(mode) {
   if (mode) {
     if (mode === "Length") {
       let failed = false;
-      let curnum = ""
-      while (!failed){
+      let curnum = "";
+      while (!failed) {
         document.querySelector(".cardholder").innerHTML = "";
-        curnum = curnum+Math.round(Math.random() * 9).toString();
-        failed = await cyclelength(curnum)
-        await function(){return new Promise((resolve)=>setTimeout(resolve,2500))}
+        curnum = curnum + Math.round(Math.random() * 9).toString();
+        failed = await cyclelength(curnum);
+        await function () {
+          return new Promise((resolve) => setTimeout(resolve, 2500));
+        };
       }
+    } else if (mode === "Cards") {
+      let selection = [undefined,undefined]
+              document.querySelector(".cardholder").innerHTML = "";
+      cards.forEach((x) =>
+        document.querySelector(".cardholder").insertAdjacentHTML(
+          "afterbegin",
+          `<div class="card">
+          <img src="cardimgs/${x}.png" alt="${x}">
+            <h1>${x}</h1>
+          </div>
+           ` 
+        )
+      );
     }
   }
 }
@@ -81,7 +94,7 @@ document.querySelectorAll(".selector").forEach((x) =>
     selectedMode = event.target
       .closest(".card")
       .querySelector("h1").textContent;
-    
+
     beginGame(selectedMode);
   })
 );
